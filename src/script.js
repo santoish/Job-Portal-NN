7e4baec66dcc7ba94dcd28764c1c9c19d383e536
santoish <santosanthoshsk81@gmail.com>
2025-04-16 15:31:09 +0530

    Homepage.html Completed

--- src/script.js
+++ src/script.js
@@ -2,3 +2,8 @@ function toggleBox(e) {
     let box = document.getElementById("box");
     e.name === 'menu-outline' ? (e.name = "close-outline", box.classList.add('top-[80px]'), box.classList.add('opacity-100')) : (e.name = "menu-outline", box.classList.remove('top-[80px]'), box.classList.remove('opacity-100'))
 }
+
+document.getElementById("submit-btn").addEventListener("click",function(e){
+    e.preventDefault();
+    alert("Submitted Successfully !");
+})
